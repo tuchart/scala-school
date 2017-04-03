5a100c85f952a500a5b6c90c83670baa533dfd93
Artem Tuchin <tuchin5@ya.ru>
2017-04-03 10:22:30 +0300

    hw2

--- src/main/scala/lectures/matching/SortingStuff.scala
+++ src/main/scala/lectures/matching/SortingStuff.scala
@@ -67,30 +67,31 @@ object SortingStuff extends App {
                       boots: List[Boots] = Nil,
                       junk: List[Stuff] = Nil)
 
-   def sortJunk(stuff: List[Stuff]): StuffBox = ??? ///sort(stuff, StuffBox())
+   def sortJunk(stuff: List[Stuff]): StuffBox = sort(stuff, StuffBox())
   // // Замените знаки вопроса подходящим кодом
   // // Поправьте логику метода
-  //  private def sort(stuff: List[Stuff], stuffBox: StuffBox): StuffBox = ??? {
-  //    case _ => stuffBox
-  //    case ??? =>
-  //      val newBox = putStuffInRightBox(item, stuffBox)
-  //      sort(rest, newBox)
-  //
-  //  }
+    private def sort(stuff: List[Stuff], stuffBox: StuffBox): StuffBox = stuff match {
+      case item :: rest =>
+        val newBox = putStuffInRightBox(item, stuffBox)
+        sort(rest, newBox)
+      case _ => stuffBox
+
+    }
   //  // Метод должен положить вещь в правильную коробку
-  //  private def putStuffInRightBox(item: Stuff, stuffBox: StuffBox) = ??? {
-  //    case ??? => stuffBox.copy(watches = it :: stuffBox.watches)
-  //    case junk@_ => stuffBox.copy(junk = junk :: stuffBox.junk)
-  //    case ??? => stuffBox.copy(boots = it :: stuffBox.boots)
-  //  }
-  //
-  //  def findMyKnife(stuffBox: StuffBox): Boolean = stuffBox match {
-  //    case ??? if junk.contains(Knife) => true
-  //    case _ => false
-  //  }
+    private def putStuffInRightBox(item: Stuff, stuffBox: StuffBox) = item match {
+      case watch@Watches(_, _) if watch.cost > 1000 => stuffBox.copy(watches = watch :: stuffBox.watches)
+      case boots@Boots(_, _) if boots.brand == "Adidas" || boots.brand == "Converse" =>  stuffBox.copy(boots = boots :: stuffBox.boots)
+      case book@Book(_, _) if book.isInteresting => stuffBox.copy(books = book :: stuffBox.books)
+      case junk@_ => stuffBox.copy(junk = junk :: stuffBox.junk)
+    }
+
+    def findMyKnife(stuffBox: StuffBox): Boolean = stuffBox match {
+      case stuffBox@StuffBox(_,_,_, junk) if junk.contains(Knife) => true
+      case _ => false
+    }
 
   // //вместо вопросов подставьте композицию функций  sortJunk и findMyKnife
-  // val knifeIsInJunk = (???) (stuff)
+   val knifeIsInJunk = (sortJunk _ andThen findMyKnife _)(stuff)
 
-  //print(s"Is knife in a junk? - $knifeIsInJunk")
+  print(s"Is knife in a junk? - $knifeIsInJunk")
 }
--- src/main/scala/lectures/operators/Competition.scala
+++ src/main/scala/lectures/operators/Competition.scala
@@ -24,21 +24,24 @@ package lectures.operators
 
 object Competition extends App {
 
-  val locals = Map("Artem" -> 6, "Sergey" -> 5, "Anton" -> 2, "Vladimir" -> "2", "Alexander" -> 4l)
+  val locals = Map("Artem" -> 6, "Sergey" -> 5, "Anton" -> 2, "Vladimir" -> 2, "Alexander" -> 41)
   val foreigners = Map[String, Int]("John" -> 3, "James" -> 1, "Tom" -> 2, "Dick" -> 5, "Eric" -> 6)
 
-  //  val results = for (l <- locals;
-  //                     ???) {
-  //    val localName = l._1
-  //    val localValue = l._2
-  //    ???
-  //  }
+    val results = for (l <- locals;
+                       f <- foreigners) yield {
+      val localName = l._1
+      val foreignName = f._1
+      val localValue = l._2
+      val foreignValue = f._2
+      (localName + " vs " + foreignName) -> (localValue - foreignValue)
+    }
 
-  //  var finalResult = 0
-  //  for(r <- results) {
-  //    if(???)finalResult = finalResult + 1
-  //    else ???
-  //  }
-
-  print("Победила дружба")
+    var finalResult = 0
+    for(r <- results) {
+      if(r._2 > 0)finalResult = finalResult + 1
+      else if (r._2 < 0) finalResult -= 1
+    }
+  if (finalResult == 0) print("Победила дружба")
+  if (finalResult > 0) print("Наша взяла")
+  if (finalResult < 0) print("Продули")
 }
